7cf6d37c6633ceccffe0815371abdd1269684716
TomKoltunov <74180693+TomKoltunov@users.noreply.github.com>
2022-03-23 12:43:17 +0200

    Update Notebook.hpp

--- Notebook.hpp
+++ Notebook.hpp
@@ -9,16 +9,16 @@ using namespace std;
 
 namespace ariel {
 
-    class Notebook {
+    class Notebook { // declaration of a class named "Notebook"
 
-        unordered_map<int, vector<vector<char>>> notebook;
+        unordered_map<int, vector<vector<char>>> notebook; // declaration of unordered map that
 
         public:
 
             Notebook();
             
             void write(int page, int row, int column, Direction direction, const string &toWrite);
-            string read(int page, int row, int column, Direction direction, int toRead);
+            static string read(int page, int row, int column, Direction direction, int toRead);
             void erase(int page, int row, int column, Direction direction, int toErase);
             void show(int page);
     };
